85c6196058c3ca2099e2ac9d0814dfe0e68f64f8
Luke Barbier <lukebarbier12@gmail.com>
2021-07-14 07:52:12 -0600

    updated simulated imu

---
+++ scripts/wit_imu_stub.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import Imu
+from nav_msgs.msg import Odometry
+
+rospy.init_node("wit_imu_stub")
+pub = rospy.Publisher("imu/data", Imu, queue_size=10)
+
+def callback(msg):
+    global pub
+
+    imu = Imu()
+    imu.header.frame_id = "odom"
+    imu.header.stamp = msg.header.stamp
+    imu.orientation = msg.pose.pose.orientation
+    pub.publish( imu )
+
+
+rospy.Subscriber("pose_gt", Odometry, callback)
+rospy.spin()
